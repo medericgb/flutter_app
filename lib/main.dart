fd7fa3efebf2aaab760b8dd87a214809c33c71ed
medericgb <medericjoel@gmail.com>
2020-12-13 12:32:53 +0000

    Quote class

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'quote.dart';
 
 void main() => runApp(MaterialApp(
       home: QuotesList(),
@@ -12,12 +13,30 @@ class QuotesList extends StatefulWidget {
 }
 
 class _QuotesListState extends State<QuotesList> {
-  List<String> quotes = [
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-    'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
-    'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+  List<Quote> quotes = [
+    Quote(
+      author: 'Jean',
+      text:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    ),
+    Quote(
+      author: 'Jack',
+      text:
+          'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    ),
+    Quote(
+      author: 'Marion',
+      text:
+          'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+    ),
+    Quote(
+      author: 'Kareem',
+      text:
+          'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    ),
   ];
+
+  List<String> authors = [];
 
   @override
   Widget build(BuildContext context) {
@@ -31,7 +50,13 @@ class _QuotesListState extends State<QuotesList> {
       body: Container(
         margin: const EdgeInsets.symmetric(horizontal: 16.0),
         child: Column(
-          children: quotes.map((quote) => Text(quote)).toList(),
+          children: quotes
+              .map(
+                (quote) => Text(
+                  '${quote.text} - écrit par ${quote.author}',
+                ),
+              )
+              .toList(),
         ),
       ),
     );
